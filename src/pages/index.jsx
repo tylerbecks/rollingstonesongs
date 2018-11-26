ad6310f16755dc367d022e54bb56d0dc16a33b25
Tyler Becks <tbecks@linkedin.com>
2018-11-25 20:05:08 -0800

    Add spotify links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import encodeurl from 'encodeurl'
 import { Card, Image } from 'semantic-ui-react'
 import Layout from '../components/layout'
 import './index.css'
@@ -10,21 +11,29 @@ const IndexPage = ({ data }) => (
       {data.allAlbumsJson.edges.map(
         ({
           node: { id, band, album, href, rank, recordLabel, year, description },
-        }) => (
-          <Card key={id}>
-            <Card.Content header={rank} />
-            <Image src={href} />
-            <Card.Content>
-              <Card.Header>{band}, <em>{album}</em></Card.Header>
-              <Card.Meta>
-                <span className="date">{year}, {recordLabel}</span>
-              </Card.Meta>
-              <Card.Description>
-                {description}
-              </Card.Description>
-            </Card.Content>
-          </Card>
-        )
+        }) => {
+          const spotifyHref = encodeurl(
+            `https://open.spotify.com/search/results/artist%3A${band} album%3A${album}`
+          )
+
+          return (
+            <Card key={id}>
+              <Image src={href} href={spotifyHref} target="_blank" />
+              <Card.Content>
+                <Card.Header content={rank} />
+                <Card.Header href={spotifyHref} target="_blank">
+                  {band}, <em>{album}</em>
+                </Card.Header>
+                <Card.Meta>
+                  <span className="date">
+                    {year}, {recordLabel}
+                  </span>
+                </Card.Meta>
+                <Card.Description>{description}</Card.Description>
+              </Card.Content>
+            </Card>
+          )
+        }
       )}
     </div>
   </Layout>
