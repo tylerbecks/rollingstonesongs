a0184f1f9ef74f21cf8cd4616948522fd023ef80
Tyler Becks <tbecks@linkedin.com>
2018-12-03 17:28:43 -0800

    Move window reference to componentDidMount

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,43 +1,42 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Card, Image, Label, Icon } from 'semantic-ui-react'
 
-const AlbumCard = ({
-  bookmarked,
-  description,
-  header,
-  href,
-  id,
-  imageHref,
-  meta,
-  subHeader,
-}) => (
-  <Card id={id}>
-    <Image src={imageHref} href={href} target="_blank" />
-    <Card.Content>
-      <Label
-        active
-        as="a"
-        content={<Icon name="bookmark" />}
-        color={bookmarked ? 'green' : 'red'}
-        disabled={bookmarked}
-        href={getBookmarkedHref(id)}
-        ribbon
-        style={{ marginBottom: 10 }}
-      />
-      <Card.Header content={header} />
-      <Card.Header href={href} target="_blank">
-        {subHeader}
-      </Card.Header>
-      <Card.Meta>{meta}</Card.Meta>
-      <Card.Description>{description}</Card.Description>
-    </Card.Content>
-  </Card>
-)
+export default class IndexPage extends PureComponent {
+  state = {
+    bookMarkHref: '',
+  }
 
-const getBookmarkedHref = id => {
-  if (typeof window === 'undefined') return ''
+  componentDidMount() {
+    // only access window in componentDidMount to ensure this code is running in the browser.  Gatsby build fails on the server side if window is referenced outside here.
+    const bookMarkHref = `${window.location.origin}/#${this.props.id}`
+    this.setState({ bookMarkHref })
+  }
 
-  return `${window.location.origin}/#${id}`
+  render() {
+    return (
+      <Card id={this.props.id}>
+        <Image
+          src={this.props.imageHref}
+          href={this.props.href}
+          target="_blank"
+        />
+        <Card.Content>
+          <Label
+            as="a"
+            content={<Icon name="bookmark" />}
+            color={this.props.bookmarked ? 'green' : 'red'}
+            href={this.state.bookMarkHref}
+            ribbon
+            style={{ marginBottom: 10 }}
+          />
+          <Card.Header content={this.props.header} />
+          <Card.Header href={this.props.href} target="_blank">
+            {this.props.subHeader}
+          </Card.Header>
+          <Card.Meta>{this.props.meta}</Card.Meta>
+          <Card.Description>{this.props.description}</Card.Description>
+        </Card.Content>
+      </Card>
+    )
+  }
 }
-
-export default AlbumCard
